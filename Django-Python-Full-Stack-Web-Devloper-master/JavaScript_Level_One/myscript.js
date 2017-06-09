a9b75d7bda6142125e00b7ced23da7e5cd7e7562
kristinadarroch <dar15003@byui.edu>
2017-06-09 17:31:25 -0600

    javascript

--- Django-Python-Full-Stack-Web-Devloper-master/JavaScript_Level_One/myscript.js
+++ Django-Python-Full-Stack-Web-Devloper-master/JavaScript_Level_One/myscript.js
@@ -1,36 +1,36 @@
-// var hot = false;
-// var temp = 50;
-//
-// if (temp>80) {
-//   console.log('hot outside');
-// }
-// else if (temp <= 80 && temp >= 50) {
-//   console.log('avg temp outside');
-// }else if (temp < 50 && temp >= 32) {
-//   console.log('it is pretty cold out');
-// }else {
-//   console.log('it is cold out');
-// }
-// console.log(hot);
+var hot = false;
+var temp = 50;
+
+if (temp>80) {
+  console.log('hot outside');
+}
+else if (temp <= 80 && temp >= 50) {
+  console.log('avg temp outside');
+}else if (temp < 50 && temp >= 32) {
+  console.log('it is pretty cold out');
+}else {
+  console.log('it is cold out');
+}
+console.log(hot);
 
 
-// var ham = 10;
-// var cheese = 10;
-//
-// var report = "blank"
-//
-// if (ham >= 10 && cheese >= 10) {
-//   report = "strong sales of both ham and cheese"
-// }else if (ham === 0 && cheese === 0) {
-//   report = "nothing sold"
-// }else {
-//   report = "we had some sales of items"
-// }
-//
-// // how you get the output in the console
-// console.log(report);
+var ham = 10;
+var cheese = 10;
 
-// outside while loop
+var report = "blank"
+
+if (ham >= 10 && cheese >= 10) {
+  report = "strong sales of both ham and cheese"
+}else if (ham === 0 && cheese === 0) {
+  report = "nothing sold"
+}else {
+  report = "we had some sales of items"
+}
+
+// how you get the output in the console
+console.log(report);
+
+outside while loop
 var x = 0
 
 // this loop keeps looping until it hits 5
@@ -48,4 +48,45 @@ if (x === 3) {
 
 // reassign x to the current x but add one
   x = x+1;
+}
+
+
+// WHILE LOOP THAT PRINTS OUT ONLY EVEN NUMBERS FROM 1-10
+
+//this program I entered in manually what is an even number and what is odd
+
+var x = 0;
+while (x < 10) {
+  if (x === 2) {
+    console.log('2');
+  }else if (x === 4) {
+    console.log('4');
+  }else if (x === 6) {
+    console.log('6');
+  }else if (x === 8) {
+    console.log('8');
+  }else if (x === 10) {
+    console.log('10');
+  }
+  // else {
+  //   console.log(x + ' is an odd number');
+  // }
+
+  x = x+1;
+
+}
+
+var x = 0;
+while (x < 11) {
+  console.log(x);
+
+  x = x+2;
+}
+
+var num = 1;
+while (num < 11) {
+  if (num%2 === 0) {
+    console.log(num);
+  }
+  num = num+1;
 }
