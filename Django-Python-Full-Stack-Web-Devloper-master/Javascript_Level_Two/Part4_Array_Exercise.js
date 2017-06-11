14f4ff2ba22b6fdd8eca7f61914962b0e3c82866
kristinadarroch <dar15003@byui.edu>
2017-06-10 18:11:30 -0600

    array

--- Django-Python-Full-Stack-Web-Devloper-master/Javascript_Level_Two/Part4_Array_Exercise.js
+++ Django-Python-Full-Stack-Web-Devloper-master/Javascript_Level_Two/Part4_Array_Exercise.js
@@ -3,17 +3,35 @@
 
 // Create Empty Student Roster Array
 // This has been done for you!
-var roster = []
+var roster = [];
 
 // Create the functions for the tasks
+function addNew(roster) {
+  var newName = prompt("What name would you like to add?");
+  roster.push(newName);
+}
+function remove(roster) {
+  var remName = prompt("What name do you like to remove?");
+  var index = roster.indexOf(remName);
+  roster.splice(index,1);
+}
+function display(roster) {
+console.log(roster);
+}
+function quit(roster) {
+
+}
 
 // ADD A NEW STUDENT
 
+
 // Create a function called addNew that takes in a name
+
 // and uses .push to add a new student to the array
 
 
 // REMOVE STUDENT
+
 
 // Create a function called remove that takes in a name
 // Finds its index location, then removes that name from the roster
@@ -30,3 +48,24 @@ var roster = []
 
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
+
+var start = prompt("would you like to start the roster web app? y/n");
+var request = "empty";
+
+if (start === 'y') {
+  while (request != 'quit') {
+    request = prompt("please select a new action: add,remove,display, or quit.");
+    if (request === 'add') {
+      addNew();
+    }else if (request === 'display') {
+      display();
+    }else if (request === 'remove') {
+      remove();
+    }else {
+      alert("not recognized");
+
+    }
+  }
+}
+
+alert("Thanks for using the web app, refresh to start over.")
