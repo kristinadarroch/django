b1ee9a4f359faad243e628b7ab5ea92dde57390a
kristinadarroch <dar15003@byui.edu>
2017-06-12 13:14:53 -0600

    game

--- Django-Python-Full-Stack-Web-Devloper-master/jQuery/front-end-project.js
+++ Django-Python-Full-Stack-Web-Devloper-master/jQuery/front-end-project.js
@@ -5,3 +5,11 @@ var playerOneColor = rgb(255,0,0); //red
 
 var playerTwo = prompt('Enter the name of player 2:');
 var playerTwoColor = rgb(0,0,255); //blue
+
+var game_on = true;
+var table = $('table tr');
+
+
+function changeColor(rowIndex,colIndex,color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
