9221871c25436ca03df6a95d791e04d628770c9c
kristinadarroch <dar15003@byui.edu>
2017-06-12 11:10:58 -0600

    connect four

---
+++ Django-Python-Full-Stack-Web-Devloper-master/jQuery/front-end-project.js
@@ -0,0 +1,7 @@
+console.log('hello connect four');
+
+var playerOne = prompt('Enter the name of player 1:');
+var playerOneColor = rgb(255,0,0); //red
+
+var playerTwo = prompt('Enter the name of player 2:');
+var playerTwoColor = rgb(0,0,255); //blue
