e82945f3e5182a0109f770854ab8c38727660b19
kristinadarroch <dar15003@byui.edu>
2017-06-18 23:04:55 -0600

    python - numbers

---
+++ Django-Python-Full-Stack-Web-Devloper-master/Python_Level_One/django-lectures/numbers.py
@@ -0,0 +1,19 @@
+# order of operations is followed in python
+
+# print(2+10 * 10+3)
+
+a = 5 #how you assign variables in python
+print(a+a)
+# python allows variable reassignment
+a = a*5
+print(a)
+
+# PYTHON VARIABLE NAMES CANNOT START WITH A NUMBER
+# CANNOT START WITH SPECIAL CHARACTERS
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+# the first number is a number the second a floating point, so python
+# automatically makes the outcome a floating point.
